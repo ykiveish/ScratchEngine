7e2aebf6f7e8021d5cbd549f699c9c518d159fab
Yevgeniy Kiveisha <yevgeniy.kiveisha@gmail.com>
2017-09-09 11:21:07 -0700

    Scratch blocks progress #1

--- Src/ScratchEngine/ScratchEngine.c
+++ Src/ScratchEngine/ScratchEngine.c
@@ -16,6 +16,19 @@
 
 #include "ROBO_TX_PRG.h"
 
+#define MAX_BRANCHES 						8
+#define MAX_SCRATCH_NODES 					256
+#define MAX_SCRATCH_SENSORS 				64
+
+#define FLOW_START 							0
+#define FLOW_END 							MAX_SCRATCH_NODES
+
+#define SCRATCH_NODE_MOTOR_ENGINE			1
+#define SCRATCH_NODE_ULTRASONIC_SENSOR		2
+#define SCRATCH_NODE_WHILE					10
+#define SCRATCH_NODE_IF						11
+#define SCRATCH_NODE_VARIABLE				12
+
 struct global_vars {
 	/* Injection - Variables. [GLOBALS] */
 };
@@ -33,27 +46,35 @@ struct scratch_motor {
 };
 
 struct scratch_node {
-	int 		type;
-	void 		*data;
-	void		*action;
-	struct node *jump;
-};
-
-struct node {
-	struct scratch_node *scratch_flow_item;
-	struct node 		*next;
+	int 				type;
+	void 				*data;
+	void				*action;
+	struct scratch_node *jump;
+	struct scratch_node *next;
 };
 
 struct sensor_db {
-	void 	*sensors[64];
+	void 	*sensors[MAX_SCRATCH_SENSORS];
 	int 	index;
 };
+
+struct flow_branch {
+	struct scratch_node *start;
+	struct scratch_node *end;
+	struct scratch_node *current;
+};
+
+struct context {
+	struct flow_branch 	branch[MAX_BRANCHES];
+	int 				branch_count;
+};
+struct context this;
 
 void
 sensor_db_init (struct sensor_db *item) {
 	int i = 0;
 	item->index = 0;
-	for (i = 0; i < 64; i++) {
+	for (i = 0; i < MAX_SCRATCH_SENSORS; i++) {
 		item->sensors[i] = NULL;
 	}
 }
@@ -67,7 +88,21 @@ sesnsor_db_add (struct sensor_db *item, void * sensor) {
 
 struct global_vars 	globals;
 struct sensor_db 	sesnor_list;
-struct scratch_node scratch_node_list[256];
+struct scratch_node scratch_node_list[MAX_SCRATCH_NODES];
+
+void
+handle_branch_flow (int branch_idx) {
+	struct scratch_node *current_node = this.branch[branch_idx].current;
+
+	switch (current_node->type) {
+		case SCRATCH_NODE_MOTOR_ENGINE:
+		break;
+		default:
+		break;
+	}
+
+	current_node = current_node->next;
+}
 
 /*-----------------------------------------------------------------------------
  * Function Name       : PrgInit
@@ -95,5 +130,12 @@ PrgInit (TA * p_ta_array, int ta_count) {
  * ta_count - number of transfer areas in array (equal to TA_COUNT)
  *-----------------------------------------------------------------------------*/
 int PrgTic (TA * p_ta_array, int ta_count) {
+	int branch_idx = 0,
+		branch_count = this.branch_count;
+
+	for (branch_idx = 0; branch_idx < branch_count; branch_idx++) {
+		handle_branch_flow (branch_idx);
+	}
+
     return 0;
 }
